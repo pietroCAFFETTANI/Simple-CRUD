059edd087179ee5929247fc7a9e31df5bf4b6100
pietroCAFFETTANI <p.caffettani@gmail.com>
2025-02-25 22:39:47 -0300

    Authorization to HTTP Methods are now dependent of a certain role

--- src/main/java/com/REST_Project/API/infra/security/SecurityConfigurations.java
+++ src/main/java/com/REST_Project/API/infra/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package com.REST_Project.API.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,11 @@ public class SecurityConfigurations {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Statless (REST)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.DELETE, "/books/{id}/delete").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/books/{id}/update").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/add").hasRole("ADMIN")
+                        .anyRequest().authenticated())//Quais são as requisições HTTP que serão authorizadas dependedo do Role
                 .build();
     }
 
